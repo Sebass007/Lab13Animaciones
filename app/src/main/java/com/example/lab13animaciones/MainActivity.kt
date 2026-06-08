19b6f727aa982f31a492113b442357ab7e87758f
Sebass007 <fortsad156@gmail.com>
2026-06-08 10:28:51 -0500

    Ejercicio 1: AnimatedVisibility con Card

--- app/src/main/java/com/example/lab13animaciones/MainActivity.kt
+++ app/src/main/java/com/example/lab13animaciones/MainActivity.kt
@@ -4,13 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.lab13animaciones.ui.theme.Lab13AnimacionesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +36,49 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab13AnimacionesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SizePositionAnimationScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun SizePositionAnimationScreen() {
+    var isExpanded by remember { mutableStateOf(false) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab13AnimacionesTheme {
-        Greeting("Android")
+     val animatedSize by animateDpAsState(
+        targetValue = if (isExpanded) 200.dp else 100.dp,
+        animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy),
+        label = "SizeAnimation"
+    )
+
+    val animatedOffset by animateDpAsState(
+        targetValue = if (isExpanded) 150.dp else 0.dp,
+        animationSpec = tween(durationMillis = 600),
+        label = "OffsetAnimation"
+    )
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { isExpanded = !isExpanded }) {
+            Text("Mueve y Cambiar el tamaño")
+        }
+
+        Box(
+            modifier = Modifier
+                .offset(y = animatedOffset)
+                .size(animatedSize)
+                .background(Color(0xFF9C27B0), shape = RoundedCornerShape(16.dp)),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "vamos!",
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+        }
     }
 }
